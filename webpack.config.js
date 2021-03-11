f6f53ca14c56a23078b246e0f577543a28c8cf5a
Hector Martinez <hectormartinezresendiz@gmail.com>
2021-03-10 20:57:42 -0600

    Soport image, video and fonts

--- webpack.config.js
+++ webpack.config.js
@@ -61,6 +61,15 @@ module.exports = {
         test: /\.(js|jsx)$/,
         use: 'babel-loader',
         exclude: /node_modules/
+      },
+      {
+        test: /\.jpg|png|gif|woff|eot|ttf|svg|mp4|webm$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 90000,
+          }
+        }
       }
     ]
   },
